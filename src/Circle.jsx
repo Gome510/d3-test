0876fd8b628d7ec82136927d0878d4c6438f8982
Gome510 <bluehawkfire55@gmail.com>
2023-10-24 22:44:36 -0700

    circle svg

---
+++ src/Circle.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+function Circle() {
+  return (
+    <svg>
+      <circle cx="150" cy="77" r="40" />
+    </svg>
+  );
+}
+
+export default Circle;
