0c066e940c7f8c15d9a3e55ed71c419eecc787a8
Gome510 <bluehawkfire55@gmail.com>
2023-10-28 22:51:43 -0700

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/d3-test/">
       <RoutePaths />
     </BrowserRouter>
   </React.StrictMode>
--- src/routes/RoutePaths.jsx
+++ src/routes/RoutePaths.jsx
@@ -10,12 +10,12 @@ import EnterExit from "../pages/examples/EnterExit";
 function RoutePaths() {
   return (
     <Routes>
-      <Route element={<App />} path="/d3-test/">
+      <Route element={<App />} path="/">
         <Route index element={<HomePage />} />
-        <Route element={<Circle />} path="./circle" />
-        <Route element={<Circles />} path="./circles" />
-        <Route element={<Dashboard />} path="./dashboard" />
-        <Route element={<EnterExit />} path="./enter-exit" />
+        <Route element={<Circle />} path="circle/" />
+        <Route element={<Circles />} path="circles/" />
+        <Route element={<Dashboard />} path="dashboard/" />
+        <Route element={<EnterExit />} path="enter-exit/" />
       </Route>
     </Routes>
   );
